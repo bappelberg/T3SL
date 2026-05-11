c9969b938985f2ff1528a76f5fa2ad9c5f7cae69
bappelberg <benjamin.w.appelberg@gmail.com>
2026-05-11 20:14:26 +0200

    point api to cloudflare tunnel

--- public/js/storage.js
+++ public/js/storage.js
@@ -1,11 +1,13 @@
+const API = "https://tech-anaheim-instrumental-mac.trycloudflare.com";
+
 const Storage = {
   async getTechnicians() {
-    const res = await fetch('/api/technicians');
+    const res = await fetch(`${API}/api/technicians`);
     return res.json();
   },
 
   async saveEntry(technicianId, category, seconds) {
-    const res = await fetch('/api/entries', {
+    const res = await fetch(`${API}/api/entries`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ technician_id: technicianId, category, seconds }),
@@ -14,7 +16,7 @@ const Storage = {
   },
 
   async getEntries() {
-    const res = await fetch('/api/entries');
+    const res = await fetch(`${API}/api/entries`);
     return res.json();
   },
 };
